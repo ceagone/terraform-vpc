29bcb576e0561d69e113bd5e01abf7b4704598aa
Bola <65468836+ceagone@users.noreply.github.com>
2020-09-28 21:13:54 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "terraform-bola"
+        bucket = "terraform-jenkins-bola"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
